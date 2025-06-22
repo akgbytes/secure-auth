bb6e2a9ddb3644b6889e78a8cfe06ce2d3d2099d
Aman Kumar Gupta <akgbytes@gmail.com>
2025-06-23 03:47:05 +0530

    fixed footer styling

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Heart } from "lucide-react";
 
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 text-zinc-100 mb-3 border-t-[0.5px] border-zinc-400 min-w-2xl">
+    <footer className="-mb-15 mt-8 sm:-mb-60 sm:mt-0 text-zinc-100 border-t-[0.5px] border-zinc-400 min-w-2xl">
       <div className="flex items-center justify-center mt-2 gap-1">
         <span className="block">
           Made with <Heart className="inline border-none" /> by
