48e9193aa15cc2f8edc5cf51c14f01b7da8e289d
Aman Kumar Gupta <akgbytes@gmail.com>
2025-09-02 12:44:12 +0530

    feat: add email worker for handling verification and password reset emails

---
+++ backend/src/queues/email/email.worker.ts
@@ -0,0 +1,24 @@
+import { logger } from "@/utils/logger";
+import { sendResetPasswordMail, sendVerificationMail } from "@/utils/mail";
+import { Worker } from "bullmq";
+import { connection } from "@/config/redis";
+
+const worker = new Worker(
+  "email",
+  async (job) => {
+    const { type, fullname, email, token } = job.data;
+
+    if (type === "verify") {
+      await sendVerificationMail(fullname, email, token);
+      logger.info({ email }, "Verification email sent to");
+    }
+
+    if (type === "reset") {
+      await sendResetPasswordMail(fullname, email, token);
+      logger.info({ email }, "Password reset email sent to");
+    }
+  },
+  { connection }
+);
+
+logger.info("Email worker started and listening for jobs...");
