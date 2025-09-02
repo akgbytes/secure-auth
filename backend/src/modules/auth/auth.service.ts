de95ffdbf6c7c11cac30e8cc020130cba76161c2
Aman Kumar Gupta <akgbytes@gmail.com>
2025-09-02 12:05:59 +0530

    feat: add registerUser service

---
+++ backend/src/modules/auth/auth.service.ts
@@ -0,0 +1,30 @@
+import { db } from "@/db";
+import { RegisterInput, hashPassword } from "@/modules/auth";
+import { userTable } from "@/db/schemas";
+import { eq } from "drizzle-orm";
+import { ApiError, HttpStatus } from "@/core";
+
+export async function registerUser({ name, email, password }: RegisterInput) {
+  const [existingUser] = await db
+    .select()
+    .from(userTable)
+    .where(eq(userTable.email, email));
+
+  if (existingUser)
+    throw new ApiError(HttpStatus.CONFLICT, "Email already registered");
+
+  const hashedPassword = await hashPassword(password);
+
+  const [user] = await db
+    .insert(userTable)
+    .values({ name, email, password: hashedPassword })
+    .returning();
+
+  if (!user)
+    throw new ApiError(
+      HttpStatus.INTERNAL_SERVER_ERROR,
+      "Failed to create user, Please try again."
+    );
+
+  return user;
+}
