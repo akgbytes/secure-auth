4d73d9ccedba41453080efadd96c1abfb622daa4
Aman Kumar Gupta <akgbytes@gmail.com>
2025-09-17 06:39:10 +0530

    feat: add drizzle orm

---
+++ server/src/db/index.ts
@@ -0,0 +1,16 @@
+import { env } from "@/config/env";
+import { logger } from "@/config/logger";
+import { sql } from "drizzle-orm";
+import { drizzle } from "drizzle-orm/neon-http";
+
+export const db = drizzle(env.DATABASE_URL);
+
+export const connectDrizzle = async () => {
+  try {
+    await db.execute(sql`SELECT 1`);
+    logger.info("Drizzle connected to the database");
+  } catch (error: any) {
+    logger.error("Drizzle failed to connect to the database");
+    process.exit(1);
+  }
+};
